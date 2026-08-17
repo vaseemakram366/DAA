555c258d98554a62d02ce900b1b49fc21a7a0b33
Vaseem Akram <vaseemakrama366@gmail.com>
2026-08-17 23:18:11 +0530

    Coded and Solved Insertion Sort

---
+++  Lab 2 Practice/4.py
@@ -0,0 +1,20 @@
+# Insertion sort
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+    return arr
+
+
+arr = [5, 3, 4, 1, 2]
+
+print("Before sorting:", arr)
+print("After sorting:", insertion_sort(arr))
